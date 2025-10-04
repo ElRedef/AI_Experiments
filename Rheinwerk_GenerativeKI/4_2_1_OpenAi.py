7e1ed38613303b3f6b96c91e7255e7d924593e8b
ElRedef <66745270+ElRedef@users.noreply.github.com>
2025-10-04 18:28:32 +0200

    Erster Versuch mit OpenAI

---
+++ Rheinwerk_GenerativeKI/4_2_1_OpenAi.py
@@ -0,0 +1,15 @@
+#%%packages
+import os
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+load_dotenv('4_2_2_groq.env')
+print("OPENAI Test\n")
+print(os.getenv('OPENAI_KEY'))
+
+
+# %%
+MODEL_NAME = "gpt-4o-mini"
+model = ChatOpenAI(model_name=MODEL_NAME, temperature=0, openai_api_key=os.getenv('OPENAI_KEY'))
+response = model.predict("What is the capital of France?")
+print(response)
+# %%
